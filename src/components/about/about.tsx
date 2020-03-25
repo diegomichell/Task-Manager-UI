5fed1e7449d010391f00e81fea2ca6c54f677ca9
Diego Michel <diego.ivan.perez.michel@gmail.com>
2020-03-25 11:10:24 -0400

    add description in about page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-const About = () => (<h1>About page</h1>);
+const About = () => (
+  <div className="about">
+    <h1>About page</h1>
+    <p>This is a web ui for <a target="_blank" href="https://github.com/diegomichell/Task-Manager">Task Manager</a> api</p>
+  </div>
+);
 
 export default About;
